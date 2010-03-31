935e5c9492351eb2abfdf052c2661b5b74f510ce
tshirtbingo <tshirtbingo@13108a9d-07da-4f2b-ae71-13c52245ad9e>
2010-03-31 01:57:27 +0000

    progress checkin
    git-svn-id: http://tshirtbingo.unfuddle.com/svn/tshirtbingo_code@5 13108a9d-07da-4f2b-ae71-13c52245ad9e

--- tshirtbingo/system/application/controllers/front.php
+++ tshirtbingo/system/application/controllers/front.php
@@ -10,12 +10,45 @@ class Front extends Controller {
 	function index()
 	{
 		$this->load->view('header');
-		$this->load->view('card');
+		$card_id = $this->card->generate_card(1); // generates a random easy card
+		$card_data = $this->card->show_card($card_id);
+		$this->load->view('card', $card_data);
 		$this->load->view('footer');
 	}
 	
-	function card($card_id)
+	function card($card_id,$big="")
 	{
+		$this->load->view('header');
+		$card_data = $this->card->show_card($card_id,$big);
+		$this->load->view('card', $card_data);
+		$this->load->view('footer');
+	}
+	
+	function easy($big="")
+	{
+		$this->load->view('header');
+		$card_id = $this->card->generate_card(1); // generates a random easy card
+		$card_data = $this->card->show_card($card_id,$big);
+		$this->load->view('card', $card_data);
+		$this->load->view('footer');
+	}
+	
+	function medium($big="")
+	{
+		$this->load->view('header');
+		$card_id = $this->card->generate_card(2); // generates a random medium card
+		$card_data = $this->card->show_card($card_id,$big);
+		$this->load->view('card', $card_data);
+		$this->load->view('footer');
+	}
+	
+	function hard($big="")
+	{
+		$this->load->view('header');
+		$card_id = $this->card->generate_card(3); // generates a random hard card
+		$card_data = $this->card->show_card($card_id,$big);
+		$this->load->view('card', $card_data);
+		$this->load->view('footer');
 	}
 }
 ?>
--- tshirtbingo/system/application/models/card.php
+++ tshirtbingo/system/application/models/card.php
@@ -6,5 +6,81 @@ class Card extends Model {
         // Call the Model constructor
         parent::Model();
     }
+	
+	function generate_card($difficulty)
+	{
+		// 1 = easy 2= medium 3=hard
+		
+		if ($difficulty == 1)
+		{
+			$this->db->select('shirt_id');
+			$this->db->where('ratio >', 65);
+			$query = $this->db->get('shirts');
+		
+			foreach ($query->result() as $row)
+			{
+				array_push($shirts, $row->shirt_id);
+			}
+			
+			$rand_shirts = array_rand($shirts, 25);
+		}
+		elseif ($difficulty == 2)
+		{
+			$this->db->select('shirt_id');
+			$this->db->where('ratio <=', 65);
+			$this->db->where('ratio >', 35);
+			$query = $this->db->get('shirts');
+		
+			foreach ($query->result() as $row)
+			{
+				array_push($shirts, $row->shirt_id);
+			}
+			
+			$rand_shirts = array_rand($shirts, 25);
+		}
+		elseif ($difficulty == 3)
+		{
+			$this->db->select('shirt_id');
+			$this->db->where('ratio <=', 35);
+			$query = $this->db->get('shirts');
+		
+			foreach ($query->result() as $row)
+			{
+				array_push($shirts, $row->shirt_id);
+			}
+			
+			$rand_shirts = array_rand($shirts, 25);
+		}
+		
+		$this->db->flush_cache();
+		
+		$card_data = array(
+               'shirts' => $rand_shirts
+            );
+
+		$this->db->insert('cards', $card_data); 
+		
+		$this->db->flush_cache();
+		
+		$this->db->select('card_id');
+		$this->db->where('shirts', $rand_shirts);
+		$query = $this->db->get('cards');
+		foreach ($query->result() as $row)
+		{
+			$card_id = $row->card_id;
+		}
+		return ($card_id);
+	}
+	
+	function show_card($card_id,$big)
+	{
+		if ($big != "")
+		{
+		}
+		else
+		{
+		}
+		return ($card_data);
+	}
 }
 ?>
