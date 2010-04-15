d78e3519dad4aaa51780bca42c9c2ac9996c8653
tshirtbingo <tshirtbingo@13108a9d-07da-4f2b-ae71-13c52245ad9e>
2010-04-15 15:28:05 +0000

    progress checkin - added zazzle shirt
    git-svn-id: http://tshirtbingo.unfuddle.com/svn/tshirtbingo_code@15 13108a9d-07da-4f2b-ae71-13c52245ad9e

--- tshirtbingo/system/application/views/header.php
+++ tshirtbingo/system/application/views/header.php
@@ -11,5 +11,5 @@
 </head>
 <body>
 <center>
-<div class="header"><nobr><img src="http://www.tshirtbingo.com/shirt.png" alt="t-shirt bingo"/><br/><span>T-Shirt Bingo</span></nobr></div>
+<div class="header"><nobr><a href="http://www.zazzle.com/t_shirt_bingo_shirt-235897325349749482" target="_new"><img src="http://www.tshirtbingo.com/shirt.png" alt="t-shirt bingo click here to buy this shirt!" border=0/></a><br/><span>T-Shirt Bingo</span></nobr></div>
 <br/>
