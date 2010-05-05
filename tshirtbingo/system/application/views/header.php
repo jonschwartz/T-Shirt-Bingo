b31fc001b3808cbf79005dca124fc4896be7053f
tshirtbingo <tshirtbingo@13108a9d-07da-4f2b-ae71-13c52245ad9e>
2010-05-05 14:07:47 +0000

    progress checkin
    git-svn-id: http://tshirtbingo.unfuddle.com/svn/tshirtbingo_code@20 13108a9d-07da-4f2b-ae71-13c52245ad9e

--- tshirtbingo/system/application/views/header.php
+++ tshirtbingo/system/application/views/header.php
@@ -11,5 +11,5 @@
 </head>
 <body>
 <center>
-<div class="header"><table width="800"><tr><td><a href="http://www.zazzle.com/t_shirt_bingo_shirt-235897325349749482" target="_new"><img src="http://www.tshirtbingo.com/shirt.png" alt="t-shirt bingo click here to buy this shirt!" border=0 width="100" height="94"/></a></td><td><span>T-Shirt Bingo</span></td></tr></table></div>
+<div class="header"><table width="800"><tr><td width="100"><a href="http://www.zazzle.com/t_shirt_bingo_shirt-235897325349749482" target="_new"><img src="http://www.tshirtbingo.com/shirt.png" alt="t-shirt bingo click here to buy this shirt!" border=0 width="100" height="94"/></a></td><td align="left"><span>T-Shirt Bingo</span></td><td align="right" valign="top" class="topnav noPrint">Home | About The Game | About Us | Contact Us</td></tr></table></div>
 <br/>
