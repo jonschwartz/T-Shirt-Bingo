da1f313915a91c6e80ccff59dec1062434ba6609
tshirtbingo <tshirtbingo@13108a9d-07da-4f2b-ae71-13c52245ad9e>
2010-10-09 15:08:51 +0000

    added google tracking code
    git-svn-id: http://tshirtbingo.unfuddle.com/svn/tshirtbingo_code@46 13108a9d-07da-4f2b-ae71-13c52245ad9e

--- tshirtbingo/system/application/views/frame.php
+++ tshirtbingo/system/application/views/frame.php
@@ -2,3 +2,24 @@
 <iframe src ="<?php echo $shirt_url;?>" width="95%" height="400px">
   <p>Your browser does not support iframes.  Please click here to buy the shirt!</p>
 </iframe>
+<!-- Google Code for look at shirt page Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1019100991;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "QCdeCOHp6QEQv_745QM";
+var google_conversion_value = 0;
+if (2.00) {
+  google_conversion_value = 2.00;
+}
+/* ]]> */
+</script>
+<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/1019100991/?value=2.00&amp;label=QCdeCOHp6QEQv_745QM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
