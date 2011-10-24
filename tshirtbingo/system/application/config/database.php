14a0a29ba1baac69f78ea849ab61d7bd0d30e754
Jon Schwartz <jschwartz@hmcelectronics.com>
2011-10-24 08:47:10 -0400

    clearing passwords before pushing to github

--- tshirtbingo/system/application/config/database.php
+++ tshirtbingo/system/application/config/database.php
@@ -38,9 +38,9 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "jonandje_tshirt";
-$db['default']['password'] = "huzzah37";
-$db['default']['database'] = "jonandje_tshirt";
+$db['default']['username'] = "";
+$db['default']['password'] = "";
+$db['default']['database'] = "";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
